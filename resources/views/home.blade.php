34983aab1ff5d1899b64fc360ff9085e7533e3dc
= <dellajannata2112@gmail.com>
2024-08-21 12:12:54 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+    <header class="header-1">
+        <img src="assets/logo.png">
+        <ul class="header-tengah">
+            <li><a>About Green Haven</a></li>
+            <li><a>Event Details</a></li>
+            <li><a>Our Sponsors</a></li>
+            <li><a>Leaderboard</a></li>
+        </ul>
+        <div class="button-container">
+            <div class="button">Plant a Mangrove</div>
+        </div>
+    </header>
+    <section class="project-section">
+        <!-- Left Section -->
+        <div class="project-info">
+            <div class="button">Green Haven Project - Mangrove</div>
+            <h1>Green Horizons Await, Join Us in Planting 10,000 Mangroves!</h1>
+            <div class="project-progress">
+                <div class="project-progress-desc">
+                    <div class="project-stats1">
+                        <img src="assets/logo-tree.png" alt="">
+                        <span class="project-amount">5,690</span>/10,000 Pohon
+                    </div>
+                    <div class="project-stats2">
+                        <a href="#" class="project-button">Support Our Mission ></a>
+                    </div>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress-fill"></div>
+                </div>
+
+            </div>
+            <img src="assets/gambar1.png" alt="Mangrove Project" class="project-image">
+        </div>
+
+        <!-- Right Section -->
+        <div class="leaderboard">
+            <h2>Leaderboard</h2>
+            <div class="leaderboard-tabs">
+                <div class="button-active">Most Donation</div>
+                <div class="button-nonactive">Most Recent</div>
+            </div>
+            <div class="leaderboard-list">
+                <div class="leaderboard-item">
+                    <div class="rank">01</div>
+                    <img src="assets/Mangrove Maven.png" alt="">
+                    <div class="user-info">
+                        <span class="badge">Mangrove Maven</span>
+                        <span class="user-name">Budi Hartanto</span>
+                    </div>
+                    <img class="logo-tree2" src="assets/logo-tree.png" alt="">
+                    <div class="donation-count">350 </div>
+                    <div class="donation-text">Pohon</div>
+                </div>
+                <div class="leaderboard-item">
+                    <div class="rank">02</div>
+                    <img src="assets/Mangrove Maven.png" alt="">
+                    <div class="user-info">
+                        <span class="badge">Mangrove Maven</span>
+                        <span class="user-name">Dewi Sartika</span>
+                    </div>
+                    <img class="logo-tree2" src="assets/logo-tree.png" alt="">
+                    <div class="donation-count">330</div>
+                    <div class="donation-text">Pohon</div>
+                </div>
+                <div class="leaderboard-item">
+                    <div class="rank">03</div>
+                    <img src="assets/Mangrove Maven.png" alt="">
+                    <div class="user-info">
+                        <span class="badge">Mangrove Maven</span>
+                        <span class="user-name">Eko Wahyudi</span>
+                    </div>
+                    <img class="logo-tree2" src="assets//logo-tree.png" alt="">
+                    <div class="donation-count">200</div>
+                    <div class="donation-text">Pohon</div>
+                </div>
+                <div class="leaderboard-item">
+                    <div class="rank">04</div>
+                    <img src="assets/Guardian of the Grove.png" alt="">
+                    <div class="user-info">
+                        <span class="badge">Guardian of the Grove</span>
+                        <span class="user-name">Rina Kartika</span>
+                    </div>
+                    <img class="logo-tree2" src="assets/logo-tree.png" alt="">
+                    <div class="donation-count">190</div>
+                    <div class="donation-text">Pohon</div>
+                </div>
+                <div class="leaderboard-item">
+                    <div class="rank">05</div>
+                    <img src="assets/Guardian of the Grove.png" alt="">
+                    <div class="user-info">
+                        <span class="badge">Guardian of the Grove</span>
+                        <span class="user-name">Agus Prasetyo</span>
+                    </div>
+                    <img class="logo-tree2" src="assets/logo-tree.png" alt="">
+                    <div class="donation-count">140</div>
+                    <div class="donation-text">Pohon</div>
+                </div>
+                <div class="leaderboard-item">
+                    <div class="rank">06</div>
+                    <img src="assets/Mangrove Master.png" alt="">
+                    <div class="user-info">
+                        <span class="badge">Mangrove Maker</span>
+                        <span class="user-name">Irfan Setiawan</span>
+                    </div>
+                    <img class="logo-tree2" src="assets/logo-tree.png" alt="">
+                    <div class="donation-count">80</div>
+                    <div class="donation-text">Pohon</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-brand">
+        <img src="assets/brand1.png" alt="">
+        <img src="assets/brand2.png" alt="">
+        <img src="assets/brand3.png" alt="">
+        <img src="assets/brand4.png" alt="">
+        <img src="assets/brand5.png" alt="">
+        <img src="assets/brand6.png" alt="">
+    </section>
+    <section class="">
+        <div class="section-about">
+            <div class="logo-about">
+                <img src="assets/logo1.png" alt="">
+                <img src="assets/logo2.png" alt="">
+            </div>
+            <h1>About Green Haven Project</h1>
+            <p class="desc-about">The Green Haven Project - Mangrove aims to restore coastal ecosystems and raise
+                awareness by planting mangroves and educating participants about the importance of these areas. This
+                six-hour event will involve JCI members, local farmers, and students, focusing on combating beach
+                erosion and promoting ESG values.</p>
+        </div>
+        <div class="about-img">
+            <div class="section-about2">
+                <img src="assets/about1.png" alt="">
+                <h2>Before mangroves were planted</h2>
+            </div>
+            <div class="section-about2">
+                <img src="assets/about2.png" alt="">
+                <h2>After mangroves were planted</h2>
+            </div>
+            <div class="arrow-center">
+                <img src="assets/arrow.png" alt="">
+                <span>TRANSFORMATION</span>
+            </div>
+        </div>        
+    </section>
+    <section class="sec-summary">
+        <div class="title-sum">
+            <h1>Event Summary</h1>
+            <p>We give you a brief overview of the event to give you a glimpse of what we're doing
+                in the Green Haven Project, download the detailed PDF to see how you can get involved</p>
+        </div>
+        <div class="card-summary">
+            <div class="summary-1">
+                <p class="tanggal">26/04/2024</p>
+                <h1 class="jam">08:00 - 11:00 WIB</h1>
+                <p class="desc">Join the impactful journey of planting mangroves,
+                    where you'll learn, collaborate with locals, earn recognition,
+                    and share your story through media coverage</p>
+                <hr>
+                <div class="btn"><img src="assets/icon1.png" alt="">View Event Details</div>
+            </div>
+
+            <div class="summary-2">
+                <img src="assets/about1.png" alt="">
+            </div>
+        </div>
+        <div class="summary-end">
+            <div class="end-text">
+                <hr class="garis-1">
+                <p class="number">1</p>
+                <p>Introducing the beauty
+                    of mangroves on a journey to the planting site.</p>
+            </div>
+            <div class="end-text">
+                <hr class="garis">
+                <p class="number">2</p>
+                <p>Working together with
+                    local farmers while
+                    planting each mangrove</p>
+            </div>
+            <div class="end-text">
+                <hr class="garis">
+                <p class="number">3</p>
+                <p>Exploring the wonders of mangrove ecotourism, an educational adventure.</p>
+            </div>
+            <div class="end-text">
+                <hr class="garis">
+                <p class="number">4</p>
+                <p>Receive a plaque & badge,
+                    a mark of honor for every tree you plant.</p>
+            </div>
+            <div class="end-text">
+                <hr class="garis">
+                <p class="number">5</p>
+                <p>Coverage & documentation by news media, share your story of change!</p>
+            </div>
+        </div>
+    </section>
+    <section class="sponsor">
+        <h1>Sponsorship Packages</h1>
+        <p>Our sponsorship package offers branding, promotional opportunities, and visibility at our event, ideal for
+            enhancing company presence and community engagement.</p>
+        <h3>Personal Sponsorship Package</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Membership Tier</th>
+                    <th>Min. Seeds</th>
+                    <th>Min. Buy</th>
+                    <th>Benefits</th>
+                    <th>Donors</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><img src="assets/Seedling Scout.png" alt="Seedling Scout">Seedling Scout</td>
+                    <td>5 Seeds</td>
+                    <td>IDR 75K</td>
+                    <td>Badge</td>
+                    <td>4,900 Donors</td>
+                    <td><button class="action-btn">Support Our Mission ></button></td>
+                </tr>
+                <tr class="block-biru">
+                    <td><img src="assets/Sapling Savior.png" alt="Sapling Savior">Sapling Savior</td>
+                    <td>10 Seeds</td>
+                    <td>IDR 150K</td>
+                    <td>Badge + Pin</td>
+                    <td>4,300 Donors</td>
+                    <td><button class="action-btn">Support Our Mission ></button></td>
+                </tr>
+                <tr>
+                    <td><img src="assets/Tree Titan.png" alt="Tree Titan">Tree Titan</td>
+                    <td>20 Seeds</td>
+                    <td>IDR 300K</td>
+                    <td>Badge + Pin</td>
+                    <td>15,000 Donors</td>
+                    <td><button class="action-btn">Support Our Mission ></button></td>
+                </tr>
+                <tr class="block-biru">
+                    <td><img src="assets/Mangrove Master.png" alt="Mangrove Master">Mangrove Master</td>
+                    <td>50 Seeds</td>
+                    <td>IDR 750K</td>
+                    <td>Badge + Pin + Bag</td>
+                    <td>5,000 Donors</td>
+                    <td><button class="action-btn">Support Our Mission ></button></td>
+                </tr>
+                <tr>
+                    <td><img src="assets/Guardian of the Grove.png" alt="Guardian of the Grove">Guardian Of The Grove
+                    </td>
+                    <td>100 Seeds</td>
+                    <td>IDR 1,500K</td>
+                    <td>Badge + Pin + Bag</td>
+                    <td>300 Donors</td>
+                    <td><button class="action-btn">Support Our Mission ></button></td>
+                </tr>
+                <tr class="block-biru">
+                    <td><img src="assets/Mangrove Maven.png" alt="Mangrove Maven">Mangrove Maven</td>
+                    <td>200 Seeds</td>
+                    <td>IDR 3,000K</td>
+                    <td>Badge + Pin + Bag</td>
+                    <td>10 Donors</td>
+                    <td><button class="action-btn">Support Our Mission ></button></td>
+                </tr>
+            </tbody>
+        </table>
+        <h3>Corporate Sponsorship Package</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Benefits</th>
+                    <th>Eco Trailblazers</th>
+                    <th>Green Innovators</th>
+                    <th>Eco Vanguard</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Price</td>
+                    <td>15 Million IDR</td>
+                    <td>30 Million IDR</td>
+                    <td>45 Million IDR</td>
+                </tr>
+                <tr>
+                    <td>CO2 Sequestration</td>
+                    <td>5 Seeds</td>
+                    <td>IDR 75K</td>
+                    <td>Badge</td>
+                </tr>
+                <tr>
+                    <td>Number of Tress</td>
+                    <td>1000 Trees</td>
+                    <td>2000 Trees</td>
+                    <td>3000 Trees</td>
+                </tr>
+                <tr>
+                    <td>Logo on JCI Banner</td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                </tr>
+                <tr>
+                    <td>Logo on Communal Stainless Plate</td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                </tr>
+                <tr>
+                    <td>Considered as Main Sponsor</td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                </tr>
+                <tr>
+                    <td>Dedicated Stainless Plate</td>
+                    <td></td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                </tr>
+                <tr>
+                    <td>Report Update on Planted Trees</td>
+                    <td></td>
+                    <td>&#10003;</td>
+                    <td>&#10003;</td>
+                </tr>
+                <tr>
+                    <td>Logo on Event Clothings</td>
+                    <td></td>
+                    <td></td>
+                    <td>&#10003;</td>
+                </tr>
+                <tr class="foot-table">
+                    <td class="foot-table1"><div class="btn-donation">Make a Donation</div></td>
+                    <td class="foot-table1"><button>Support Our Mission &gt;</button></td>
+                    <td class="foot-table1"><button>Support Our Mission &gt;</button></td>
+                    <td class="foot-table1"><button>Support Our Mission &gt;</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+    <section class="donation-section">
+        <div class="content">
+            <h1>Donate Today and be a Guardian of the Green Belt.</h1>
+            <p>Join us in our mission to rejuvenate and protect our shorelines by donating to our mangrove planting
+                project.</p>
+            <a href="#" class="donate-button">Support Our Mission</a>
+        </div>
+        <div class="images-grid">
+            <img src="assets/donate1.png" alt="Mangrove Planting 1">
+            <img src="assets/donate2.png" alt="Mangrove Planting 2">
+            <img src="assets/donate3.png" alt="Mangrove Planting 3">
+            <img src="assets/donate4.png" alt="Mangrove Planting 4">
+            <img src="assets/donate5.png" alt="Mangrove Planting 5">
+        </div>
+    </section>
+</body>
+
+</html>
